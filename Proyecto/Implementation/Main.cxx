f713e5aee384182dda70b575a83e3af15656cbdf
dcortesantonio <dcortesantonio@gmail.com>
2020-04-06 00:32:47 -0500

    Add files via upload

--- Proyecto/Implementation/Main.cxx
+++ Proyecto/Implementation/Main.cxx
@@ -4,6 +4,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <stdio.h>
+#include <string>
 // The header files for performing input and output.
  
 using namespace cv;
@@ -31,27 +32,75 @@ int main(int argc, char** argv )
 
   // Read an image
   
-    Mat src, img_gray, imgBlu,imgCanny;
+  Mat src, img_gray, imgBlu,imgCanny,imgThreshold;
+
+  vector< vector<Point> > contours; // List of contour points (External)
+	vector<Vec4i> hierarchy;
+  //ReAd the image
 	src = imread( argv[1], IMREAD_COLOR);
-//src = imread( "Level-1.1.jpg", IMREAD_COLOR);
+  stringstream ss( argv[ 1 ] );
+  string basename;
+  getline( ss, basename, '.' );
   if ( !src.data )
   {
     std::cerr << "Error: No image data" << std::endl;
     return( -1);
   
-  } // fi
+  }
   
-	//SE PASA LA IMAGEN A ESCALA DE GRISES
+	//Convert image to gray
 	cvtColor(src, img_gray, cv::COLOR_BGR2GRAY);
-   GaussianBlur(img_gray,            // input image
-        imgBlu,                            // output image
-        cv::Size(5, 5),                        // smoothing window width and height in pixels
-        1.5);                                // sigma value, determines how much the image will be blurred
 
-    Canny(imgBlu,            // input image
-        imgCanny,                    // output image
+  //Apply Gaussian Blur to grayscaled image.
+   GaussianBlur(img_gray,            
+        imgBlu,                            
+        cv::Size(5, 5), //Kernel size                      
+        1.5);                                
+
+    Canny(imgBlu,            
+        imgCanny,                    
         100,                        // low threshold
         200);                        // high threshold
-        imwrite( "imgBlu.png", imgBlu );
-        imwrite( "Borders.png", imgCanny );   
+
+    imwrite( basename + "GaussianBlur.png", imgBlu );
+    imwrite( basename + "__AllBorders.png", imgCanny );   
+    
+    //Binary Thresholding
+    threshold( imgBlu, 
+        imgThreshold, 
+        127,   // low threshold
+        255,   // high threshold
+        THRESH_BINARY_INV);
+      
+    imwrite( basename + "_Binary.png", imgThreshold );
+
+     //Find contours of the image.(External)
+    findContours(imgThreshold, 
+        contours, 
+        hierarchy, 
+        CV_RETR_EXTERNAL,
+        CHAIN_APPROX_SIMPLE,
+        Point(0, 0));
+
+  //Bordes: where i'm going to draw the contour.
+  Mat Borders= Mat::zeros(imgThreshold.size(), CV_8UC3);
+
+ Scalar color_contours( rand()&0, rand()&255, rand()&0 ); //Green
+ //Draw the contour.
+ for(int i = 0; i < contours.size(); i++) 
+    drawContours(Borders, 
+        contours, 
+        i, 
+        color_contours, 
+        1, 
+        8, 
+        vector<Vec4i>(),
+        2, 
+        Point());
+	   
+  
+  imwrite( basename + "_Border.png", Borders );
+
+  
+
 }
--- Proyecto/Implementation/Prueba.cxx
+++ Proyecto/Implementation/Prueba.cxx
@@ -8,20 +8,21 @@
 
 using namespace cv;
 using namespace std;
-
-Mat src; Mat src_gray;
+/*
+Mat src; 
+Mat src_gray;
 int thresh = 100;
 int max_thresh = 255;
 RNG rng(12345);
 
 /// Function header
 void thresh_callback(int, void* );
-
+*/
 /** @function main */
 int main( int argc, char** argv )
 {
   /// Load source image and convert it to gray
- 
+ Mat src;
   	src = imread( argv[1], IMREAD_COLOR);
 //src = imread( "Level-1.1.jpg", IMREAD_COLOR);
   if ( !src.data )
@@ -31,37 +32,50 @@ int main( int argc, char** argv )
   
   } // fi
   /// Convert image to gray and blur it
-  cvtColor( src, src_gray, CV_BGR2GRAY );
-  blur( src_gray, src_gray, Size(3,3) );
+  Mat gray, blur_image, threshold_output;
+  
+	cvtColor(src, gray, COLOR_BGR2GRAY); // convert to grayscale
 
+	blur(gray, blur_image, Size(3, 3)); // apply blur to grayscaled image
 
-  createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
-  thresh_callback( 0, 0 );
+	threshold(blur_image, threshold_output, 127, 255, THRESH_BINARY_INV);
+ stringstream ss( argv[ 1 ] );
+  string basename;
+  getline( ss, basename, '.' );
+  imwrite( basename +"BinaryInv.jpg", threshold_output );
+  vector< vector<Point> > contours; // list of contour points
+	vector<Vec4i> hierarchy;
 
-  waitKey(0);
+	// find contours
+	//findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+	findContours(threshold_output, contours, hierarchy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
+   // create hull array for convex hull points
+
+	vector< vector<Point> > hull(contours.size());
+
+	for(int i = 0; i < contours.size(); i++)
+
+	    convexHull(Mat(contours[i]), hull[i], false);
+// create a blank image (black image)
+
+	Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
+
+	for(int i = 0; i < contours.size(); i++) 
+  {
+
+	    //Scalar color_contours = Scalar(0, 255, 0); // green - color for contours
+      Scalar color_contours( rand()&0, rand()&255, rand()&0 );
+	    //Scalar color = Scalar(255, 0, 0); // blue - color for convex hull
+      //Scalar color( rand()&255, rand()&0, rand()&0 );
+       
+	    // draw ith contour
+	    drawContours(drawing, contours, i, color_contours, 1, 8, vector<Vec4i>(), 2, Point());
+	    // draw ith convex hull
+	    //drawContours(drawing, hull, i, color, 1, 8, vector<Vec4i>(), 0, Point());
+
+	}
+  
+  imwrite( basename + "Border.jpg", drawing );
+
   return(0);
-}
-
-/** @function thresh_callback */
-void thresh_callback(int, void* )
-{
-  Mat canny_output;
-  vector<vector<Point> > contours;
-  vector<Vec4i> hierarchy;
-
-  /// Detect edges using canny
-  Canny( src_gray, canny_output, thresh, thresh*2, 3 );
-  /// Find contours
-  findContours( canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-  /// Draw contours
-  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-  for( int i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-     }
-
-  /// SAbe the image
-  imwrite( "Contours.jpg", drawing );
 }
